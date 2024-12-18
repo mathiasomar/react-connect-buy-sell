b42a6fc6069e3c695650a96a6c2610d48e576612
mathiasomar <ommykmathy@gmail.com>
2024-12-18 15:23:46 +0300

    creating category menu constant

---
+++ client/src/constatnts/index.jsx
@@ -0,0 +1,38 @@
+export const heroCategory = [
+  {
+    id: 1,
+    img: "images/vehicle_icon.png",
+    name: "vehicles",
+    path: "/vehicles",
+  },
+  {
+    id: 2,
+    img: "images/fashion_icon.png",
+    name: "fashions",
+    path: "/fashions",
+  },
+  {
+    id: 1,
+    img: "images/jobs-1.png",
+    name: "jobs",
+    path: "jobs",
+  },
+  {
+    id: 1,
+    img: "images/real_estate_icon.png",
+    name: "real estate",
+    path: "estate",
+  },
+  {
+    id: 1,
+    img: "images/services_icon.png",
+    name: "services",
+    path: "/services",
+  },
+  {
+    id: 1,
+    img: "images/pets_icon.png",
+    name: "pets",
+    path: "/pets",
+  },
+];

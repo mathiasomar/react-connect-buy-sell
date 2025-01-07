1b8ca1b0d543ccbf34a630a3420c3e48518a02b6
mathiasomar <ommykmathy@gmail.com>
2025-01-07 11:26:12 +0300

    creating listing details

--- client/src/scenes/ViewListing.jsx
+++ client/src/scenes/ViewListing.jsx
@@ -1,9 +1,17 @@
+import Container from "../components/Container"
+import PageHeader from "../components/pageHeader"
 import Section from "../components/section"
 
 const ViewListing = () => {
   return (
     <Section className="min-h-screen">
-      
+      <Container>
+        <PageHeader page="Listing Details" />
+      </Container>
+
+      <Container className="py-[100px]">
+
+      </Container>
     </Section>
   )
 }
